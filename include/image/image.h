e10ada7c2065b365e345c73ba43c48e9f1ad4e54
jeffbrn <jeff.brn@gmail.com>
2023-08-19 09:02:28 -0600

    fix and cleanup

--- include/image/image.h
+++ include/image/image.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <array>
-#include <string_view>
+#include <string>
 #include <Eigen/Dense>
 #include <cstdint>
 
@@ -9,15 +9,18 @@ namespace jeffb::cv::image {
 
 class Image {
 public:
-    bool load(std::string_view);
-    std::array<int,2> get_dims() const { return {dim_x_,dim_y_}; }
+    bool load(std::string);
+
+    [[nodiscard]]
+    std::array<int, 2> get_dims() const { return {dim_x_, dim_y_}; }
+
     Eigen::Ref<Eigen::Array<uint8_t, Eigen::Dynamic, Eigen::Dynamic>> get_image() { return img_; }
 
 private:
-    int dim_x_ {0};
-    int dim_y_ {0};
-    int orig_chans_ {0};
-    Eigen::Array<uint8_t, Eigen::Dynamic, Eigen::Dynamic> img_ = Eigen::Array<uint8_t, Eigen::Dynamic, Eigen::Dynamic>::Zero(0,0);
+    int dim_x_{0};
+    int dim_y_{0};
+    Eigen::Array<uint8_t, Eigen::Dynamic, Eigen::Dynamic> img_ = Eigen::Array<uint8_t, Eigen::Dynamic, Eigen::Dynamic>::Zero(
+            0, 0);
 };
 
 }
--- src/image/image.cpp
+++ src/image/image.cpp
@@ -1,20 +1,21 @@
 #include "image/image.h"
 
 #define STB_IMAGE_IMPLEMENTATION
+
 #include "common/stb_image.h"
 
 using namespace Eigen;
 
 namespace jeffb::cv::image {
 
-bool Image::load(std::string_view) {
+bool Image::load(std::string filepathname) {
     int chans;
-    auto buff = stbi_load("../imgs/Chapter2/Input/Square.png", &dim_x_, &dim_y_, &chans, 1);
+    auto buff = stbi_load(filepathname.c_str(), &dim_x_, &dim_y_, &chans, 1);
     if (buff == nullptr) {
         return false;
     }
     auto buff_len = dim_x_ * dim_y_ * 1;
-    auto data = static_cast<uint8_t*>(buff);
+    auto data = static_cast<uint8_t *>(buff);
     img_ = Map<Array<uint8_t, Dynamic, Dynamic>, RowMajor>(data, dim_y_, dim_x_);
     stbi_image_free(buff);
     return true;
